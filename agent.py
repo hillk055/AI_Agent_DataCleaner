7b7b01e3a9f89fe2005d0185444bf8f9df3c6a55
hillk055 <101193869+hillk055@users.noreply.github.com>
2025-10-02 14:56:54 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -16,75 +16,79 @@ class Clean:
         self.df = df
         self.columns = df.columns
 
-    def get_dtypes(self):
+    @staticmethod
+    def remove_delimiters(x):
 
-        print(self.df.dtypes)
+        delimiters = [',', '.', '/', '-', ' ']
+        for dlm in delimiters:
+            if dlm in x:
+                x = x.replace(dlm, '')
+        return x
 
     def format_date(self):
 
-        date_cols = [x for x in self.df.columns if 'date' in x]
-        for column in date_cols:
-            self.df[column] = self.df[column].apply(lambda x: str(x))
+        for col in self.df.columns:
 
-        date_format = ['_'] * 8
-        remaining = ''
+            day_month_format = ''
+            date_format = ['_'] * 8
+            remaining = ''
 
-        for j in date_cols:
-            for x in self.df[j]:
-                opt1_year, opt2_year = x[:4], x[4:]
-                opt1_year_int, opt2_year_int = int(opt1_year), int(opt2_year)
-                if 1900 <= opt1_year_int <= 2099:
+            if 'date' in col:
+                self.df[col] = self.df[col].astype(str).str.zfill(8)
 
-                    remaining = x.replace(opt1_year, '')
-                    date_format[:4] = 'Y' * 4
+                values = self.df[col]
+                values = [self.remove_delimiters(x) for x in values]
 
-                if 1900 <= opt2_year_int <= 2099:
+                num_date_formats = set([len(x) for x in values])
+                if len(num_date_formats) != 1:
+                    raise ValueError('Mismatching date formats')
 
-                    remaining = x.replace(opt2_year, '')
+                for x in values:
 
-                split = len(remaining)//2
-                one, two = remaining[:split], remaining[split:]
+                    opt1_year, opt2_year = x[:4], x[4:]
+                    opt1_year_int, opt2_year_int = int(opt1_year), int(opt2_year)
 
-                if int(one) > 12:
-                    date_format[4:6] = ['d'] * 2
-                    date_format[6:] = ['m'] * 2
+                    if 1900 <= opt1_year_int <= 2099 and 1900 <= opt2_year_int <= 2099:
+                        continue
 
+                    elif 1900 <= opt1_year_int <= 2099:
+                        remaining = x.replace(opt1_year, '')
+                        date_format[:4] = 'Y' * 4
+
+                    elif 1900 <= opt2_year_int <= 2099:
+                        remaining = x.replace(opt2_year, '')
+                        date_format[4:] = 'Y' * 4
+
+                    split = len(remaining) // 2
+                    one, two = remaining[:split], remaining[split:]
+
+                    if int(one) == int(two) or (int(one) < 12 and int(two) < 12):
+                        continue
+
+                    elif int(one) > 12:
+
+                        day_month_format = f'ddmm'
+
+                    elif int(two) > 12:
+
+                        day_month_format = f'mmdd'
+
+                    find_filler = [pos for pos, x in enumerate(date_format) if x == '_']
+                    first_filler, last_filler = find_filler[0], find_filler[-1] + 1
+
+                    date_format[first_filler:last_filler] = day_month_format
+
+                    seen = set()
+                    unique = [x for x in date_format if not (x in seen or seen.add(x))]
+                    ft = '%'.join(unique)
+                    ft = f'%{ft}'
+
+                    self.df[col] = list(map((lambda x: datetime.strptime(x, ft).strftime("%Y-%m-%d")), values))
                     break
-                elif int(two) > 12:
-                    date_format[4:6] = ['m'] * 2
-                    date_format[6:] = ['d'] * 2
-                    break
 
-                else:
-                    continue
+    def return_df(self):
 
-            # If x is not in seen x in seen will return False
-            seen = set()
-            unique = [x for x in date_format if not (x in seen or seen.add(x))]
-            ft = '%'.join(unique)
-            ft = f'%{ft}'
-
-            for column in date_cols:
-
-                self.df[column] = self.df[column].apply(lambda x: datetime.strptime(x, ft).date())
-
-            print(self.df[date_cols])
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def drop_cols(self):
-
-        pass
+        return self.df['tourney_date']
 
 
 if __name__ == "__main__":
@@ -93,3 +97,5 @@ if __name__ == "__main__":
     dataframe = pd.read_csv(pth, nrows=100)
     clean = Clean(dataframe)
     clean.format_date()
+    df = clean.return_df()
+    print(df)
